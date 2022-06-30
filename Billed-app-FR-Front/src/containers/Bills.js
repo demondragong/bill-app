d22cf77dab86ab1a5c8ea08e53840fac1f37dace
Gael <demondragong@gmail.com>
2022-06-30 19:43:00 +0200

    fix minor bugs

--- Billed-app-FR-Front/src/containers/Bills.js
+++ Billed-app-FR-Front/src/containers/Bills.js
@@ -21,10 +21,10 @@ export default class {
   }
 
   handleClickIconEye = (icon) => {
-    const billUrl = icon.getAttribute("data-bill-url")
+    const billUrl = icon.dataset.billUrl
     const imgWidth = Math.floor($('#modaleFile').width() * 0.5)
     $('#modaleFile').find(".modal-body").html(`<div style='text-align: center;' class="bill-proof-container"><img width=${imgWidth} src=${billUrl} alt="Bill" /></div>`)
-    $('#modaleFile').modal('show')
+    if (typeof $('#modaleFile').modal === 'function') $('#modaleFile').modal('show')
   }
 
   getBills = () => {
