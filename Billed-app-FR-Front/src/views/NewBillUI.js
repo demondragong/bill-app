903cbe9902f5ad9da15430ab087635546bfb1be2
Gael <demondragong@gmail.com>
2022-07-05 21:54:20 +0200

    add wrong extension message

--- Billed-app-FR-Front/src/views/NewBillUI.js
+++ Billed-app-FR-Front/src/views/NewBillUI.js
@@ -56,6 +56,7 @@ export default () => {
                   <div class="col-half">
                     <label for="file" class="bold-label">Justificatif</label>
                     <input required type="file" accept=".png, .jpg, .jpeg" class="form-control blue-border" data-testid="file" />
+                    <span class="text-danger d-none" data-testid="file-extension-error">Veuillez sélectionner une image au format jpg, jpeg ou png.</span>
                   </div>
                 </div>
             </div>
